2accd3f51dbb1f27cf64b9ccc50e185a67f7f439
Arshad Hossain <arshad417hossain@gmail.com>
2022-06-22 09:03:43 +0600

    connect the nosql databse mongo db
    connect the nosql databse mongo db

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const products = require('./data/products')
+import  express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import colors from 'colors'
+import products from './data/products.js'
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
@@ -21,5 +25,5 @@ app.get('/api/products/:id', (req, res) => {
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, (req, res)=> {
-    console.log(`server is running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+    console.log(`server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
 })
